6043a87f49515610f69b22a9cdf11918e14601c2
Bruno Moreira <bruno7moreira@gmail.com>
2021-06-04 18:16:31 +0100

    RecyclerView das promoções e começo da recyclerview do produto individual

--- app/src/main/java/com/example/app_compras/promotionsFragment.kt
+++ app/src/main/java/com/example/app_compras/promotionsFragment.kt
@@ -5,55 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_promotions.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [promotionsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class promotionsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    lateinit var textViewNoPromotionsAvailable: TextView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_promotions, container, false)
+
+        textViewNoPromotionsAvailable = view.findViewById(R.id.textViewNoPromotionsAvailable)
+
+        fetchJogos {
+            if(it.size == 0) {
+                view.recyclerViewPromotions.adapter = PromotionsAdapter(it)
+                view.recyclerViewPromotions.layoutManager = LinearLayoutManager(activity)
+                textViewNoPromotionsAvailable.isVisible = true
+            } else {
+                view.recyclerViewPromotions.adapter = PromotionsAdapter(it)
+                view.recyclerViewPromotions.layoutManager = LinearLayoutManager(activity)
+                textViewNoPromotionsAvailable.isVisible = false
+            }
         }
+
+        return view
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_promotions, container, false)
-    }
+    private fun fetchJogos(myCallback: (ArrayList<Product>) -> Unit) {
+        val arrayProducts = ArrayList<Product>()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment promotionsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            promotionsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        FirebaseFirestore.getInstance().collection("products")
+            .get()
+            .addOnCompleteListener {
+                if(it.isSuccessful) {
+                    for(document in it.result!!) {
+                        if (document.getString("promotion").toString() == "1"){
+                            val item = Product(document.data.getValue(("imageUrl")).toString(),
+                                document.data.getValue(("name").toString()) as String,
+                                document.getString(("price")) as String)
+                            arrayProducts.add(item)
+                        }
+                    }
+                    myCallback(arrayProducts)
                 }
             }
     }
+
 }
