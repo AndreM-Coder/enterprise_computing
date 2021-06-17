d5d2b8bc6acde268d8ab0ec5983c26d75291caf6
Andreffff <a034875@ismai.pt>
2021-06-17 11:15:28 +0100

    Aualizacao

--- app/src/main/java/com/example/app_compras/ProductFragment.kt
+++ app/src/main/java/com/example/app_compras/ProductFragment.kt
@@ -33,7 +33,7 @@ class ProductFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
 
-        val view = inflater.inflate(R.layout.fragment_product_promotion, container, false)
+        val view = inflater.inflate(R.layout.fragment_product, container, false)
 
         textViewProductNameIndividual = view.findViewById(R.id.textViewProductNameIndividual)
         textViewProductPriceIndividual = view.findViewById(R.id.textViewProductPriceIndividual)
